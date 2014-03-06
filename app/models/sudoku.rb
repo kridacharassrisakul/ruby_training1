c8c742831273f49db50ca5656860690b8ddbaec1
kridacharassrisakul <kridac@revenuesciencecorp.com>
2014-03-06 14:35:20 +0700

    Order by Id Details

--- app/models/sudoku.rb
+++ app/models/sudoku.rb
@@ -1,5 +1,5 @@
 class Sudoku < ActiveRecord::Base 
-  has_many :details, :dependent => :destroy
+  has_many :details, order: 'id', :dependent => :destroy
   accepts_nested_attributes_for :details
   validates_associated :details
   validates :name, presence: true
